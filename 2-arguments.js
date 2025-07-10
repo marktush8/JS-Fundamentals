98edd4c29bc77dd1e357f96de2798eeb72a85111
marktush8 <tushmarceym4@gmail.com>
2025-07-10 22:36:36 +0300

    lets see

---
+++ 2-arguments.js
@@ -0,0 +1,9 @@
+function hasArg (arg) {
+    if (arg = -1) {
+        console.log ("No argument")
+    } else if (arg = 0) {
+        console.log ("Argument found")
+    } else {
+        console.log ("Arguments found")
+    }
+}
