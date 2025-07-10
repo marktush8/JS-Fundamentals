b208353d95d563743e84b8b33e7a600437cd5186
marktush8 <tushmarceym4@gmail.com>
2025-07-10 22:25:02 +0300

    correct now

--- 1-multi_languages.js
+++ 1-multi_languages.js
@@ -1,7 +1,7 @@
-const langC = "C is fun";
-const langP = "Python is cool";
-const langJ = "Javascript is amazing";
-
+let langC = "C is fun";
 console.log(langC);
+let langP = "Python is cool";
 console.log(langP);
+let langJ = "JavaScript is amazing";
 console.log(langJ);
+
