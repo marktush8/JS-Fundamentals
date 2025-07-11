88dcc80d0b9ee45881fc9f49fd4812d8d17e4f3f
marktush8 <tushmarceym4@gmail.com>
2025-07-11 08:55:25 +0300

    works now

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,7 +1,7 @@
 let argsCount = process.argv.length - 2;
 
 if (argsCount === 0) {
-    console.log("No argeument");
+    console.log("No argument");
 } else if (argsCount === 1) {
     console.log("Argument found");
 } else {
