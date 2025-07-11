0d6dd6f3f6531b7ca23966eaf605fd327ea324bd
marktush8 <tushmarceym4@gmail.com>
2025-07-11 17:45:49 +0300

    lets go

---
+++ 7-multi_c.js
@@ -0,0 +1,10 @@
+const x = number(process.argv[2]);
+
+if (Number.isInteger(x)) {
+   for (let i = 0; i < x; i++) {
+       console.log("C is fun");
+   } 
+}else {
+        console.log("Missing number of occurrences");
+   }
+
