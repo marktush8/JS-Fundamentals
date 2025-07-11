cca59b192b7feeea73d5330fec7ba17c3f6a004b
marktush8 <tushmarceym4@gmail.com>
2025-07-11 21:01:38 +0300

    try again

--- 9-add.js
+++ 9-add.js
@@ -1,7 +1,7 @@
-const a = Number(process.argv[2]);
-const b = Number(process.argv[3]);
 
 function add(a, b) {
+    const a = Number(process.argv[2]);
+const b = Number(process.argv[3]);
     return a + b;
 
     console.log (add(a, b));    
