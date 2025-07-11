8dc5b3878c4efb3b6317b60d54318c9eaf1de47b
marktush8 <tushmarceym4@gmail.com>
2025-07-11 22:17:06 +0300

    lets go

--- 4-concat.js
+++ 4-concat.js
@@ -1,6 +1,3 @@
 const argument1 = process.argv[2];
 const argument2 = process.argv[3];
-
-if (argument1 !== undefined && argument2 !== undefined) {
-    console.log("${argument1} is ${argument2}");
-}
+console.log(`${argument1} is ${argument2}`);
